758827cafc1cc8006df3d6907296dc7c67020607
tsoleary <tsoleary12@gmail.com>
2020-09-28 21:51:57 -0400

    calc mU/mL adh activity

--- ADH/scripts/biovision_init_script.R
+++ ADH/scripts/biovision_init_script.R
@@ -8,9 +8,14 @@
 require(tidyverse)
 
 # Load data
-nadh <- read_csv(here::here("ADH/data/biovision/NADH_std_09212020.csv")) 
+nadh <- read_csv(here::here("ADH/data/biovision/NADH_std_09212020.csv"))
+#nadh <- read_csv(here::here("ADH/data/biovision/NADH_std_09282020.csv"))
 
 
+abs <- read_csv(here::here("ADH/data/biovision/adh_abs_09212020.csv"))
+abs <- read_csv(here::here("ADH/data/biovision/adh_abs_09282020.csv"))
+
+# Tidy NADH data
 x <- nadh %>%
   pivot_longer(cols = -NADH, names_to = "mins", values_to = "abs") %>%
   mutate(mins = as.numeric(str_remove_all(mins, "_min"))) %>%
@@ -29,7 +34,39 @@ ggplot(data = x, aes(x = NADH, y = abs)) +
                         label.x = 0.85, 
                         label.y = "top") +
   theme_classic() +
-  labs(y = "Absorbance @ 450 nm", x = "NADH (nmol)") 
+  labs(y = "Absorbance @ 450 nm", x = "NADH (nmol)")
 
+
+# Tidy ADH abs data
+y <- abs %>%
+  pivot_longer(cols = contains("min"), names_to = "mins", values_to = "abs") %>%
+  mutate(mins = as.numeric(str_remove_all(mins, "_min")),
+         EtOH = as.factor(EtOH)) %>%
+  group_by(EtOH, sample_vol, n_flies, genotype, mins) %>%
+  summarize(abs = median(abs)) %>%
+  filter(n_flies == 5) %>%
+  filter(mins != 0) %>%
+  filter(sample_vol == 5)
+
+y1 <- y %>%
+  mutate(nmol_NADH = (abs - 0.123) / 0.0683) %>%
+  mutate(deltaAbs = abs - abs[mins == 10],
+         deltaNADH = nmol_NADH - nmol_NADH[mins == 10],
+         deltaTime = mins - 10) %>%
+  filter(deltaTime != 0) %>%
+  mutate(mU_mL = (deltaNADH/(deltaTime*(sample_vol/1000)))*9)
+  
+  
+  pivot_wider(names_from = mins, values_from = abs)
+  summarize()
+
+# Plot the absorbance over time
+ggplot(data = y, 
+       aes(x = mins, y = abs, 
+                     color = EtOH)) +
+  geom_smooth(method='lm', formula = y ~ x, se = FALSE) +
+  geom_point() +
+  theme_classic() +
+  labs(y = "Absorbance @ 450 nm", x = "Time (mins)") 
 
 
